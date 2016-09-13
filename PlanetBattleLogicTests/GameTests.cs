d38d9a26a7263deeb8f7008d10ea8b67e9084a04
David Giard <djgiard@hotmail.com>
2016-09-12 20:02:25 -0700

    Initial revision

---
+++ PlanetBattleLogicTests/GameTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlanetBattleLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetBattleLogic.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void FightBattleTest()
+        {
+
+            Assert.Fail();
+        }
+    }
+}
