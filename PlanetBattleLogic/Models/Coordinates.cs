68ce9e95f35c6937ad1af3425316d55c6919911a
David Giard <djgiard@hotmail.com>
2016-09-12 21:34:43 -0700

    Added code to move ship

--- PlanetBattleLogic/Models/Coordinates.cs
+++ PlanetBattleLogic/Models/Coordinates.cs
@@ -8,6 +8,15 @@ namespace PlanetBattleLogic.Models
 {
     public class Coordinates
     {
+        public Coordinates()
+        {
+
+        }
+        public Coordinates(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
         public double X { get; set; }
         public double Y { get; set; }
 
---
+++ PlanetBattleLogic/Utilities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlanetBattleLogic.Models;
+
+namespace PlanetBattleLogic
+{
+    public static class Utilities
+    {
+        public static double DistanceToDestination(Coordinates start, Coordinates end)
+        {
+            var xDiff = end.X - start.X;
+            var yDiff = end.Y - start.Y;
+            var diff = Math.Sqrt((xDiff * xDiff) + (yDiff * yDiff));
+
+            return diff;
+        }
+
+        public static double GetAngleOfLineBetweenTwoPoints(Coordinates start, Coordinates end)
+        {
+            double xDiff = end.X - start.X;
+            double yDiff = end.Y - start.Y;
+            return Math.Atan2(yDiff, xDiff) * (180 / Math.PI);
+        }
+
+        public static Coordinates GetDestinationLocation(Coordinates startLocation, double angle, double distance)
+        {
+            angle = Math.PI * angle / 180.0;
+            var newX = startLocation.X + (distance * Math.Cos(angle));
+            var newY = startLocation.Y + (distance * Math.Sin(angle));
+            var endLocation = new Coordinates(newX, newY);
+
+            return endLocation;
+
+        }
+    }
+}
